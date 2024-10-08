bf9ec9a9d6bd8ca1d8873b74ea4579741d50dd38
Nathan Rydel <nathanrydel@gmail.com>
2024-10-07 22:23:32 -0700

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/ui.ts',
@@ -11,7 +12,15 @@ export default {
   },
   plugins: [
     nodeResolve(),
-    typescript(),
-    terser()
+    typescript({
+      exclude: ['**/tests/**','**/*.test.ts']
+    }),
+    terser(),
+    copy({
+      targets: [
+        { src: 'public/index.html', dest: 'dist' },
+        { src: 'public/styles.css', dest: 'dist' },
+      ]
+    })
   ]
 };
